b73d8df8a747608900624bb519dc79947e00fec4
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2024-12-07 13:09:01 +0530

    Update Maximum Sum of Distinct Subarrays With Length K

--- 2461.java
+++ 2461.java
@@ -1,31 +1,33 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        
         int n = nums.length;
-        Map<Integer, Integer> count = new HashMap<>();
-        long sum = 0, maxSum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < k; i++) {
-            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+        long sum = 0;
+        long maxSum = 0;
+
+        for(int i = 0; i < k; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             sum += nums[i];
         }
 
-        if (count.size() == k) {
+        if(map.size() == k){
             maxSum = sum;
         }
 
-        for (int i = k; i < n; i++) {
-            // Remove the outgoing element
-            count.put(nums[i - k], count.get(nums[i - k]) - 1);
-            if (count.get(nums[i - k]) == 0) {
-                count.remove(nums[i - k]);
+        for(int i = k; i < n; i++){
+
+            //Remove the outgoing element from the array
+            map.put(nums[i-k], map.getOrDefault(nums[i-k], 1) - 1);
+            if(map.get(nums[i-k]) == 0){
+                map.remove(nums[i-k]);
             }
+            
+            //Add the incomming element
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            sum += nums[i] - nums[i-k];
 
-            // Add the incoming element
-            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
-            sum = sum + nums[i] - nums[i - k];
-
-            if (count.size() == k) {
+            if(map.size() == k) {
                 maxSum = Math.max(maxSum, sum);
             }
         }
