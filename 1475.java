6a3fa8035982b9d162ef73d7ab70620169413ac2
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2024-12-18 21:20:27 +0530

    Update Final Prices With a Special Discount in a Shop

--- 1475.java
+++ 1475.java
@@ -3,15 +3,11 @@ class Solution {
         int[] result = prices.clone();
 
         for(int i = 0; i < prices.length - 1; i++) {
-            int index = i;
             for(int j = i + 1; j < prices.length; j++) {
                 if(prices[j] <= prices[i]) {
-                    index = j;
+                    result[i] = prices[i] - prices[j];
                     break;
                 }
-            }
-            if(index != i) {
-                result[i] = prices[i] - prices[index];
             }
         }
         return result;
