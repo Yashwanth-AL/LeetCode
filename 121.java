acf38a6e5413076e8918fc98e7ca0679dc6ac837
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2025-01-10 22:55:19 +0530

    Update Best Time to Buy and Sell Stock

--- 121.java
+++ 121.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int minPrice = prices[0];
-        for(int i = 1; i < prices.length; i++){
-            int profit = prices[i] - minPrice;
-            if(prices[i] < minPrice){
-                minPrice = prices[i];
-            }
-            if(profit > maxProfit){
-                maxProfit = profit;
+        int currentPrice = prices[0];
+        int profit = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] > currentPrice) {
+                profit = Math.max(profit, prices[i] - currentPrice); 
+            } else {
+                currentPrice = prices[i];
             }
         }
-        return maxProfit < 0  ? 0 : maxProfit;
+        return profit;
     }
 }
