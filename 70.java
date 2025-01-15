6eb66e70965f0944490b76c7e9d7a9b502fdd904
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2025-01-15 15:01:32 +0530

    Update Climbing Stairs

--- 70.java
+++ 70.java
@@ -2,13 +2,13 @@ class Solution {
     public int climbStairs(int n) {
         if(n == 1) return 1;
         if(n == 2) return 2;
-        int prev2 = 1;
-        int prev1 = 2;
+        int current = 2;
+        int prev = 1;
         for(int i = 3; i <= n; i++) {
-            int current = prev2 + prev1;
-            prev2 = prev1;
-            prev1 = current;
+            int next = prev + current;
+            prev = current;
+            current = next;
         }
-        return prev1;
+        return current;
     }
 }
