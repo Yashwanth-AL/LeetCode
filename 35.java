35791979f758855b2b27f5617fc235443143bdf1
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2025-02-05 14:52:39 +0530

    Update Search Insert Position

--- 35.java
+++ 35.java
@@ -1,18 +1,19 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        return binarySearch(nums, 0, nums.length-1, target);
-    }
-    public static int binarySearch(int arr[], int beg, int end, int key){
-        int mid = (beg + end) / 2;
-        if(beg > end){
-            return end+1;
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) return mid;
+            
+            else if(target > nums[mid]){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
         }
-        if (arr[mid] == key){
-            return mid;
-        } else if(key > arr[mid]){
-            return binarySearch(arr, mid+1, end, key);
-        } else {
-            return binarySearch(arr, beg, mid-1, key);
-        }
+        
+        return start;
     }
 }
