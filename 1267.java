591638947afa99eed029dede144a536c807552b3
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2025-03-20 00:00:21 +0530

    Update Count Servers that Communicate

--- 1267.java
+++ 1267.java
@@ -1,24 +1,22 @@
 class Solution {
     public int countServers(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[] rowCount = new int[m];
-        int[] colCount = new int[n];
+        int[] row = new int[m];
+        int[] col = new int[n];
         
-        // Count servers in each row and column
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    rowCount[i]++;
-                    colCount[j]++;
+                    row[i]++;
+                    col[j]++;
                 }
             }
         }
         
         int count = 0;
-        // Count servers that can communicate
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1)) {
+                if (grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)) {
                     count++;
                 }
             }
