5ab883d76e153e82efa6ac7e3bff05a9a86f9060
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2025-04-17 22:54:03 +0530

    Next Greater Element II

--- 503.py
+++ 503.py
@@ -3,10 +3,26 @@ class Solution:
         n = len(nums)
         arr = [-1] * n
 
-        for i in range(n):
-            for j in range(n - 1):
-                index = (i + j + 1) % n
-                if nums[index] > nums[i]:
-                    arr[i] = nums[index]
-                    break
+        stack = []
+        for i in range(2 * n - 1, -1, -1):
+            while stack and stack[-1] <= nums[i % n]:
+                stack.pop()
+
+            if i < n:
+                arr[i] = stack[-1] if stack else -1
+
+            stack.append(nums[i % n])
+        
         return arr
+
+
+        # n = len(nums)
+        # arr = [-1] * n
+
+        # for i in range(n):
+        #     for j in range(n - 1):
+        #         index = (i + j + 1) % n
+        #         if nums[index] > nums[i]:
+        #             arr[i] = nums[index]
+        #             break
+        # return arr
