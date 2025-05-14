45dabdcc0585054dcee789e817566ff538e1c360
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2025-05-14 15:19:02 +0530

    Valid Parenthesis String

---
+++ 678.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low = 0;
+        int high = 0;
+
+        for(char ch : s.toCharArray()) {
+            if(ch == '(') {
+                low++;
+                high++;
+            } else if (ch == ')') {
+                if(low > 0) low--;
+                high--;
+            } else {
+                if(low > 0) low--;
+                high++;
+            }
+            if (high < 0) return false;
+        }
+        return low == 0;
+    }
+}
