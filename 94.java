a5d7e6aa8deb8da6ef13b2b2d1eac0cd5ee5b712
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2025-05-20 19:34:35 +0530

    Binary Tree Inorder Traversal

--- 94.java
+++ 94.java
@@ -2,14 +2,10 @@ class Solution {
     List<Integer> result = new ArrayList<>();
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        traverse(root);
-        return result;
-    }
-
-    public void traverse(TreeNode root) {
         if(root == null) return;
-        traverse(root.left);
+        inorderTraversal(root.left);
         result.add(root.val);
-        traverse(root.right);
+        inorderTraversal(root.right);
+        return result;
     }
 }
