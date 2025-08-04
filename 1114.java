677ee6d156008791c061be6da03f404575e258e6
Yashwanth A L <160408470+Yashwanth-AL@users.noreply.github.com>
2025-08-04 15:48:35 +0530

    Print in Order

---
+++ 1114.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.Semaphore;
+
+class Foo {
+
+    private Semaphore sem1;
+    private Semaphore sem2;
+
+    public Foo() {
+        sem1 = new Semaphore(0);
+        sem2 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        sem1.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        sem1.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        sem2.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        sem2.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+        sem2.release();
+    }
+}
